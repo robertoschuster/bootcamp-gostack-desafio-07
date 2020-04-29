b9883de87d5e09272720fa6ddfbee227e22ae71b
robertoschuster <robertoaschuster@gmail.com>
2020-04-29 17:02:04 -0300

    Corrigida a cor da status bar.

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Routes from './routes';
 import NavigationService from './services/navigation';
+import colors from './styles/colors';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="light-content" backgroundColor="#191920" />
+      <StatusBar barStyle="light-content" backgroundColor={colors.dark} />
       <Routes
         ref={(navigatorRef) => NavigationService.setNavigator(navigatorRef)}
       />
